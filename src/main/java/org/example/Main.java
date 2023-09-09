5dba43f0abacf430a54e5267bb86acdaf36d5641
chanyeong <22200197@handong.ac.kr>
2023-09-09 15:31:38 +0900

    menu&add option

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("*** 영단어 마스터 **\n");
+        System.out.println("*** 영단어 마스터 ***\n");
 
         new WordManager().start();
 
