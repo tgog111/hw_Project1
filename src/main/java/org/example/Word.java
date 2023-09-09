97500c1b6f9c6d5826077e6fb8adf89060142557
chanyeong <22200197@handong.ac.kr>
2023-09-09 16:55:53 +0900

    lsit option

--- src/main/java/org/example/Word.java
+++ src/main/java/org/example/Word.java
@@ -7,6 +7,8 @@ public class Word {
     private String meaning;
 
     Word(){}
+
+
     Word(int id, int level, String word, String meaning){
         this.id = id;
         this.level = level;
@@ -44,5 +46,18 @@ public class Word {
 
     public void setMeaning(String meaning) {
         this.meaning = meaning;
+    }
+
+    @Override
+    public String toString() {
+
+        String slevel = "";
+        for(int i=0 ; i<level ; i++) slevel += "*";
+
+        String str = String.format("%-3s",slevel)
+                +String.format("%15s",word) + "  "
+                +meaning;
+
+        return str;
     }
 }
